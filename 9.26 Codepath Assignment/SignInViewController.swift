33cb6f80f111f7fabfb9240c033e7ba987fc9edd
kathrinevbecker <kathrinevbecker@gmail.com>
2016-09-25 16:29:56 -0700

    9.25 4:30 version

---
+++ 9.26 Codepath Assignment/SignInViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignInViewController.swift
+//  9.26 Codepath Assignment
+//
+//  Created by Becker, Kathrine V on 9/25/16.
+//  Copyright © 2016 Becker, Kathrine V. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func didPressBackWelcome(_ sender: AnyObject) {
+        navigationController!.popViewController(animated: true)
+    }
+}
