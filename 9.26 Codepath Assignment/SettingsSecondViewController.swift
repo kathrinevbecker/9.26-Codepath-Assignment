75971fa3e81470394439ced8b68150952eb928b0
kathrinevbecker <kathrinevbecker@gmail.com>
2016-09-25 21:07:32 -0700

    fixed asset size and scrollview issues

---
+++ 9.26 Codepath Assignment/SettingsSecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsSecondViewController.swift
+//  9.26 Codepath Assignment
+//
+//  Created by Becker, Kathrine V on 9/25/16.
+//  Copyright © 2016 Becker, Kathrine V. All rights reserved.
+//
+
+import UIKit
+
+class SettingsSecondViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.contentSize = CGSize(width: 375, height: 1000)
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
